06b8f87c5968bdb836bed1f1f7d907af16febc82
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-10-22 23:44:43 +0530

    Basic Structure Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DiagramCanvasComponent } from './diagram-canvas/diagram-canvas.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiagramCanvasComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/diagram-canvas/diagram-canvas.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diagram-canvas',
+  templateUrl: './diagram-canvas.component.html',
+})
+export class DiagramCanvasComponent {}
