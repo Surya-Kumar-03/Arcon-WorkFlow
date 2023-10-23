ff189c5524cd45315609d9146aad892d79bd15d6
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-10-23 11:55:03 +0530

    Initial Structure with Editor Done

--- bpmn-js-app/src/app/app.module.ts
+++ bpmn-js-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { DiagramComponent } from './diagram/diagram.component';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DiagramComponent
+    DiagramComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
